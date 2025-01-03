b009474363bca5cb467cf41f279c826e1b756632
sanju536 <sunnysanju532@gmail.com>
2025-01-03 12:15:04 +0530

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Demo2{
+    public static void main(String[] args) {
+        int num1=30;
+        int num2=500;
+        if(num1>num2){
+            System.out.println("num1 is greater");
+        }
+        else{
+            System.out.println("num1 is smaller"); 
+        }
+        }
+    }
+
