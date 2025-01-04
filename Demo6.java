88e206d18b198772067a44ccf57d1affafac336b
sanju536 <sunnysanju532@gmail.com>
2025-01-04 12:28:36 +0530

    Add files via upload

---
+++ Demo6.java
@@ -0,0 +1,10 @@
+public class Demo6 {
+    public static void main(String[] args) {
+        String name = "Sanjay";
+        String password = "user_001";
+        while (name == "Sanjay" && password == "user_001") {
+            System.out.println("Hello User, Welcome to MALLAREDDYUNIVERSITY");
+            break;
+        }
+    }
+}
